62b16f268b29e5f561fc027f82da0f178586108d
Ratnesh <rktssgk@gmail.com>
2023-05-04 21:08:23 +0530

    basic app

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { ApolloProvider } from 'react-apollo';
-import client from './client';
+import React from "react";
+import { ApolloProvider } from "react-apollo";
+import client from "./client";
+import Screen from "./Screen";
 
 const App = () => (
   <ApolloProvider client={client}>
-    hii
+    <Screen />
   </ApolloProvider>
 );
 
--- Screen.js
+++ Screen.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import "./Screen.css"
 
-import { useQuery } from 'react-apollo';
-import { gql } from 'apollo-boost';
-
+import { useQuery } from "react-apollo";
+import { gql } from "apollo-boost";
+import client from "./client";
 
 const GET_COUNTRIES = gql`
   {
@@ -15,7 +15,7 @@ const GET_COUNTRIES = gql`
 `;
 
 const GET_COUNTRY_DETAILS = gql`
-  query Country($countryCode: String!) {
+  query Country($countryCode: ID!) {
     country(code: $countryCode) {
       name
       native
@@ -31,10 +31,67 @@ const GET_COUNTRY_DETAILS = gql`
   }
 `;
 
-const Screen = async () => {
+
+const Screen = () => {
+  const [country, setCountry] = useState("IN");
+  
+  const [detail, setDetail] = useState();
+ 
+  const { data, loading, error } = useQuery(GET_COUNTRIES);
+
+  useEffect(()=>{
+    getDetails("IN")
+  },[])
+
+  const getDetails = async (country) => {
+    const { data } = await client.query({
+      query: GET_COUNTRY_DETAILS,
+      variables: { countryCode: country },
+    });
+    setDetail(data.country)
+    console.log(data.country)
+  };
   return (
-  <p>Screen</p>
-);
-}
+    <>
+      {data ? (
+        <section >
+          <div>
+          <div class="card">
+            Select Country
+            <select
+              name="pets"
+              id="pet-select"
+              onChange={(e) => {
+                setCountry(e.target.value);
+                getDetails(e.target.value);
+              }}
+              defaultValue={country}
+            >
+              {data.countries.map((country, i) => {
+                return (
+                  <option key={i} value={country.code}>
+                    {country.name}
+                  </option>
+                );
+              })}
+            </select>
+            </div>
+            <div class="card">
+            
+                <h1>Country Details : {detail.code}</h1>
+                <p class="title">Name : {detail.name}</p>
+                <p>Phone Code : {detail.phone}</p>
+                <p>Currency : {detail.currency}</p>
+                <p>Native : {detail.native}</p>
+            </div>  
+          </div>
+        </section>
+        
+      ) : (
+        <>Loading</>
+      )}
+    </>
+  );
+};
 
 export default Screen;
